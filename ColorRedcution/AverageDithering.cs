721ca35888f04dbd01fcb64b454ab37bc76c01b7
Kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2024-12-08 00:03:57 +0100

    average dithering

--- ColorRedcution/AverageDithering.cs
+++ ColorRedcution/AverageDithering.cs
@@ -10,26 +10,19 @@ namespace ColorRedcution
     {
         public Bitmap GetReducedBitmap(Bitmap image, int kr, int kg, int kb)
         {
-            // Create a new bitmap to store the modified image
             Bitmap modifiedImage = new Bitmap(image.Width, image.Height);
 
-            // Iterate through each pixel in the image
             for (int x = 0; x < image.Width; x++)
             {
                 for (int y = 0; y < image.Height; y++)
                 {
-                    // Get the original pixel color
                     Color originalColor = image.GetPixel(x, y);
 
-                    // Apply average dithering to each color component
                     byte redComponent = (byte)AverageDitherComponent(originalColor.R, kr);
                     byte greenComponent = (byte)AverageDitherComponent(originalColor.G, kg);
                     byte blueComponent = (byte)AverageDitherComponent(originalColor.B, kb);
 
-                    // Create the modified pixel color
-                    Color modifiedColor = Color.FromArgb(originalColor.A, redComponent, greenComponent, blueComponent);
-
-                    // Set the modified pixel in the new bitmap
+                    Color modifiedColor = Color.FromArgb(redComponent, greenComponent, blueComponent);
                     modifiedImage.SetPixel(x, y, modifiedColor);
                 }
             }
@@ -39,11 +32,9 @@ namespace ColorRedcution
 
         private int AverageDitherComponent(int component, int k)
         {
-            // Calculate the number of discrete levels for the color component
-            int levels = 256 / k;
-
-            // Calculate the average dithered value for the component
-            return (component / levels) * levels + levels / 2;
+            int step = 255 / (k - 1);
+            int level = 255 / k;
+            return (component / level) * step;
         }
     }
 }
---
+++ ColorRedcution/OrderedDitheringRelative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorRedcution
+{
+    public class OrderedDitheringRelative : IColorReducer
+    {
+        public Bitmap GetReducedBitmap(Bitmap originalImage, int kr, int kg, int kb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
