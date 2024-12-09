193effd324169024f4744694f56f5bc82cd54b1d
Kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2024-12-09 19:11:04 +0100

    hsv

--- ColorRedcution/Form1.cs
+++ ColorRedcution/Form1.cs
@@ -1,5 +1,6 @@
 using System.Drawing.Imaging;
 using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ColorRedcution
 {
@@ -138,7 +139,46 @@ namespace ColorRedcution
                 stripeWidth /= 2;
             }
 
+            int hslWidth = bmp.Width - stripeStart;
+            int hslStart = stripeStart;
+
+            for (int i = 0; i < hslWidth; i++)
+            {
+                double hue = ((double)i / (double)hslWidth) * 360.0;
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    double sat = 1 - ((double)j / (double)bmp.Height);
+                    Color col = ColorFromHSV(hue, sat, 1);
+                    bmp.SetPixel(i + hslStart, j, col);
+                }
+            }
+
             return bmp;
+        }
+
+        private static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
         }
 
         private void createButton_Click(object sender, EventArgs e)
